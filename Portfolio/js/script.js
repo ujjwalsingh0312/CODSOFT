bd97c299318cdbb0dd41b4752d3ccda095a41e58
ujjwalsingh0312 <92951690+ujjwalsingh0312@users.noreply.github.com>
2023-10-03 20:35:18 +0530

    Add files via upload

---
+++ Portfolio/js/script.js
@@ -0,0 +1,61 @@
+let menuIcon = document.querySelector('#menu-icon');
+let navbar = document.querySelector('.navbar');
+menuIcon.onclick = () => {
+    menuIcon.classList.toggle('bx-x');
+    navbar.classList.toggle('active');
+};
+
+
+let sections = document.querySelectorAll('section');
+let navLinks = document.querySelectorAll('header nav a');
+window.onscroll = () => {
+    sections.forEach(sec => {
+        let top = window.scrollY;
+        let offset = sec.offsetTop - 150;
+        let height = sec.offsetHeight;
+        let id = sec.getAttribute('id');
+        if (top >= offset && top < offset + height) {
+            navLinks.forEach(links => {
+                links.classList.remove('active');
+                document.querySelector('header nav a[href*='+ id +']').classList.add('active');
+            });
+        };
+    });
+
+    let header = document.querySelector('header');
+    header.classList.toggle('sticky', window.scrollY > 100);
+
+    menuIcon.classList.remove('bx-x');
+    navbar.classList.remove('active');
+};
+
+
+var tablinks = document.getElementsByClassName("tab-links");
+var tabcontents = document.getElementsByClassName("tab-contents");
+function opentab(tabname){
+    for(tablink of tablinks){
+        tablink.classList.remove("active-link");
+    }
+    for(tabcontent of tabcontents){
+        tabcontent.classList.remove("active-tab");
+    }
+    event.currentTarget.classList.add("active-link");
+    document.getElementById(tabname).classList.add("active-tab");
+};
+
+const scriptURL = 'https://script.google.com/macros/s/AKfycbzbF8O4gQjlcX3flzyeqvcsMTMwMKD_UL-OUP1ZijG7E8sCl6f6D1nUbvmPiBHx-BVt-g/exec';
+const form = document.forms['submit-to-google-sheet'];
+const msg = document.getElementById("msg");
+
+form.addEventListener('submit', e => {
+    e.preventDefault()
+    fetch(scriptURL, { method: 'POST', body: new FormData(form)})
+    .then(response => {
+            form.reset();
+            msg.innerHTML = "Message sent Successfully, \n Thanks for Contacting!";
+            setTimeout(function(){
+                msg.innerHTML = "";
+            },5000);
+        })
+        .catch(error => console.error('Error!', error.message))
+});
